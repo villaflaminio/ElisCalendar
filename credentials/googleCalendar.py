e01b910586db30d96618b1ceeb07bcfa4337df3f
Flaminio <73552053+villaflaminio@users.noreply.github.com>
2020-11-02 10:00:06 +0100

    first commit

---
+++ credentials/googleCalendar.py
@@ -0,0 +1,228 @@
+from __future__ import print_function
+import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import time
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/calendar',"https://www.googleapis.com/auth/calendar.events"]
+calendarID= "h9i2ija6t1g1me2vgrmq6afhfg@group.calendar.google.com"
+
+def getConn():
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('credentials/token.pickle'):
+        with open('credentials/token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials/credentials.json', SCOPES)
+            creds = flow.run_local_server(port=8080)
+        # Save the credentials for the next run
+        with open('credentials/token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('calendar', 'v3', credentials=creds )
+    return  service
+
+
+
+def printEvents(resoultNumber=999999):
+    service = getConn()
+    # Call the Calendar API for events
+    now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+    print('Getting the upcoming',resoultNumber,' events')
+    try:
+        events_result = service.events().list(calendarId=calendarID,maxResults=resoultNumber, singleEvents=True,orderBy='startTime').execute()
+    except:
+        print(datetime.datetime.now()," Error getting events list !")
+
+    events = events_result.get('items', [])
+
+    if not events:
+        print('No upcoming events found.')
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        print(start, event['summary'] ,"id:",event['id'])
+
+
+
+def getEvents():
+    service = getConn()
+    # Call the Calendar API for events
+    now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+    try:
+        events_result = service.events().list(calendarId=calendarID,maxResults=99999, singleEvents=True,orderBy='startTime').execute()
+    except:
+        print(datetime.datetime.now()," Error getting events list !")
+
+    events = events_result.get('items', [])
+    return events
+
+
+def insertEvent(subject,startDateTime,endDateTime):
+    service = getConn()
+    event = { #setting up the obj event
+        'summary': subject,
+        'start': {
+            'dateTime': startDateTime,
+            'timeZone': 'Europe/Rome',
+        },
+        'end': {
+            'dateTime': endDateTime,
+            'timeZone': 'Europe/Rome',
+        },
+    }
+    insEvent = False
+    retry_n=0
+    while insEvent == False:
+        try:
+            event = service.events().insert(calendarId=calendarID, body=event).execute()
+            insEvent = True
+        except:
+            retry_n+=1
+            if(retry_n >=3):
+                insEvent = True
+            print(datetime.datetime.now(), " Error insert in calendar: ", calendarID, ": Event:", event)
+            time.sleep(4)#if something happens stop for 4 seconds , let the api refresh the time counter
+    #print('Event created: %s' % (event.get('htmlLink')))
+    return event.get('htmlLink')
+
+def insertListEvent(listEvents):
+    service = getConn()
+    addedEvents=0
+    print("\n\nTime Extimated: ",round(len(listEvents)/3),"seconds")
+    null_events =0
+    for event in listEvents:
+        time.sleep(0.1)
+        if(event["Subject"] != "x"):
+            new_event = insertEvent(event["Subject"],event["StartDateTime"],event["EndDateTime"])
+            addedEvents += 1
+            print(addedEvents, "/", len(listEvents), ' - Event created: %s' % (new_event))
+        else:
+            null_events+=1
+    print("Total Events to add:",len(listEvents)," , Events added:",addedEvents, " , Null events: "+str(null_events))
+
+
+
+def clearCalendarFromList(events_to_delete):
+    service = getConn()
+    i=0
+    eventslength = len(events_to_delete)
+    for event in events_to_delete:
+        time.sleep(0.1)
+        print("Event: ", i, "/", len(events_to_delete), "Event id: ", event["id"])
+        delEvent = True
+        num_retry = 0
+        while delEvent == True:
+            try:
+                delev = service.events().delete(calendarId=calendarID, eventId=event["id"]).execute()
+                delEvent = False
+            except Exception as er:
+                num_retry += 1
+                print(datetime.datetime.now(), " Error deleting from calendar: ", calendarID, ": Err:", er)
+                if (num_retry >= 3):
+                    delEvent = False
+                else:
+                    delEvent = True
+                time.sleep(4)  # if something happens stop for 4 seconds , let the api refresh the time counter
+        i += 1
+    print("Elementi da eliminare:", eventslength, " , Elementi eliminati : ", i)
+
+def clearCalendar(service):
+    i=0
+    try:
+        events_result = service.events().list(calendarId=calendarID, maxResults=99999, singleEvents=True,orderBy='startTime').execute()
+    except ValueError as er:
+        print(datetime.datetime.now()," Error getting the list! er: "+str(er))
+        return
+    events = events_result.get('items', [])
+    eventslength=len(events)
+    if not events:
+        print('No events found.')
+    for event in events:
+        time.sleep(0.1)
+        print("Event: ",i,"/",len(events),"Event id: ",event["id"])
+        delEvent = True
+        num_retry=0
+        while delEvent==True:
+            try:
+                delev = service.events().delete(calendarId=calendarID ,eventId=event["id"]).execute()
+                delEvent=False
+            except Exception as er:
+                num_retry+=1
+                print(datetime.datetime.now()," Error deleting from calendar: ",calendarID,": Err:",er)
+                if(num_retry>=3):
+                    delEvent=False
+                else:
+                    delEvent=True
+                time.sleep(4)#if something happens stop for 4 seconds , let the api refresh the time counter
+        i+=1
+    print("Elementi esistenti:",eventslength," , Elementi eliminati : ",i)
+
+
+def clearCalendarFromToday(service):
+    try:
+        events_result = service.events().list(calendarId=calendarID, maxResults=99999 ,singleEvents=True,orderBy='startTime').execute()
+    except:
+        print(datetime.datetime.now()," Error getting the list!")
+        return
+    events = events_result.get('items', [])
+
+    now= datetime.datetime.now()
+    indexFrom = 0
+    i=0
+
+    for event in events:
+        if( datetime.datetime.strptime(event["start"]["dateTime"][:-6], "%Y-%m-%dT%H:%M:%S") >= now - datetime.timedelta(days=2)):
+            print("TrovatoOggi")
+            break
+        else:
+            indexFrom+=1
+
+    eventslength=len(events)
+    print("Update events from:",indexFrom ,"to",eventslength)
+    print("Time Extimated: ",round((eventslength-indexFrom)/3),"seconds")
+
+    newEvents = events[indexFrom:]
+    inizio = indexFrom
+    if not newEvents:
+        print('No events found.')
+    for event in newEvents:
+        time.sleep(0.1)
+        print("Event: ", i, "/", len(events), "Event id: ", event["id"])
+        delEvent = True
+        num_retry = 0
+        while delEvent == True:
+            try:
+                delev = service.events().delete(calendarId=calendarID, eventId=event["id"]).execute()
+                delEvent = False
+            except Exception as er:
+                num_retry += 1
+                print(datetime.datetime.now(), " Error deleting from calendar: ", calendarID, ": Err:", er)
+                if (num_retry >= 3):
+                    delEvent = False
+                else:
+                    delEvent = True
+                time.sleep(4)  # if something happens stop for 4 seconds , let the api refresh the time counter
+        indexFrom += 1
+        i+=1
+
+    print("Elementi totali:",eventslength,"| Elementi da eliminare : ",len(newEvents)," , Elementi eliminati",i)
+    return inizio
+
+
+if __name__ == '__main__':
+    getConn()
---
+++ main.py
@@ -0,0 +1,154 @@
+import xlrd
+import xlwt
+import datetime
+import time
+import requests
+import ssl
+from  credentials import googleCalendar as myCalendar
+import json
+
+#WebSSL certificates
+ssl._create_default_https_context = ssl._create_unverified_context
+#download the xlsx file from the link
+link = "https://cedelcloud-my.sharepoint.com/:x:/g/personal/g_tomeno_elis_org/EdfyxBa5gSFNivc2-aj7G0IB48bm609wlXrkbQc7ITLoww?download=1"
+
+#Defining vars
+listEventi = []
+#insert the time range
+Orario = ["8,45-9,45", "9,45-10,45", "10,45-11,45", "11,45-12,45", "14,15-15,15", "15,15-16,15", "16,15-17,15","17,15-18,15"]
+
+
+def getList():
+    print("Getting the list")
+    #Downloading the file
+    webFile = requests.get(link)
+
+    #Open Workbook
+    wb = xlrd.open_workbook(file_contents=webFile.content)
+
+    #check the sheet index I want
+    namesSheets = wb.sheet_names()
+    sheetIndex=0
+    for name in namesSheets:
+        if(name == "2 anno AA 20-21" ):
+            break
+        else:
+            sheetIndex+=1
+
+    print("Sheet 2 anno AA 20-21 index:",sheetIndex)
+
+    #select the index of the sheet
+    sheet = wb.sheet_by_index(sheetIndex)
+
+    # the first month of the calendar, 10 = october
+    startMonth = 10
+    # select the year
+    startYear = 2020
+
+    #the number of the month in the exel file
+    monthNumber = 10
+    hourNumber = 7
+
+    for z in range(0, monthNumber): #for each month = z
+        currentColumnMonth = z * 10
+        if (startMonth == 13): #startMonth is a int and when it goes over 12 i need to reset the year
+            startMonth = 1
+            startYear += 1
+
+        for i in range(sheet.nrows): # i= row
+            if (i != 0 and i<35): # i need to skip the first blank row and 
+                if (sheet.cell_value(i, currentColumnMonth) != ""): #check the value if it is not null
+                    dataNum = int(sheet.cell_value(i, currentColumnMonth))
+                    completeData = [int(startYear),int(startMonth),int(dataNum)]
+
+                    # cycle foreach hour
+                    j = 2 #increment number for getting the right column
+                    for n in range(0, hourNumber): # number of hours of lessons
+                        subject = sheet.cell_value(i, j + currentColumnMonth) # get the subject
+                        Time = (sheet.cell_value(1, j + currentColumnMonth)).split("-") #get the time splitted
+                        #splitting the unic time in start and end time
+                        startTime = Time[0].split(",")
+                        endTime =Time[1].split(",")
+
+                        #creating the datetime string formatted
+                        startDateTime = datetime.datetime(int(completeData[0]),int(completeData[1]),int(completeData[2]),int(startTime[0]),int(startTime[1]),0).strftime("%Y-%m-%dT%H:%M:%S")
+                        endDateTime = datetime.datetime(int(completeData[0]),int(completeData[1]),int(completeData[2]),int(endTime[0]),int(endTime[1])+1,0).strftime("%Y-%m-%dT%H:%M:%S")
+
+                        if (subject and subject!="x"): # i there is a subject i add the event
+                            listEventi.append({"Subject":subject, "StartDateTime":startDateTime,"EndDateTime":endDateTime})
+                        j += 1 # i move a column further for getting the next hour
+        startMonth += 1 # increment the int month counter
+
+def elementExists(event,google_list):
+    for google_event in google_list:
+        if(event["Subject"]==google_event["Subject"] and event["StartDateTime"]==google_event["StartDateTime"] and event["EndDateTime"]==google_event["EndDateTime"]):
+            return True
+    return False
+
+def removeElementGoogleList(event_to_remove,new_google_list):
+    for event_google in new_google_list:
+        if(event_to_remove["Subject"]==event_google["Subject"] and event_to_remove["StartDateTime"]==event_google["StartDateTime"] and event_to_remove["EndDateTime"]==event_google["EndDateTime"]):
+            new_google_list.remove(event_google)
+
+    return new_google_list
+
+def updateList(updated_list_events):
+    # get the list from google and restructure for a fast match
+    list_events_google = myCalendar.getEvents()
+    new_google_list = []
+    for event in list_events_google:
+        new_google_list.append({"Subject": event["summary"], "StartDateTime": event["start"]["dateTime"][:-6],
+                                "EndDateTime": event["end"]["dateTime"][:-6],"id":event["id"]})
+    # now i have 2 list one is: updated_list_events  and the other is new_google_list
+
+    element_to_add = []
+
+    for event in updated_list_events:
+        if elementExists(event,new_google_list):
+            removeElementGoogleList(event,new_google_list) # if i find the element i remove from the google list
+            # in the google list will remain only the elements to delete
+        else:
+            element_to_add.append(event)  # add the events which doesn't exists
+
+    element_to_delete = new_google_list  # all the items which remain in the list need to be deleteid
+
+    print("ELEMENT TO DELETE: [\n", element_to_delete, "\n\n]")
+    if(len(element_to_delete)>0):
+        myCalendar.clearCalendarFromList(element_to_delete)
+    else:
+        print("List is up-to date, no event need to be deleted")
+
+    print("ELEMENT TO ADD: [\n", element_to_add, "\n\n]")
+    if(len(element_to_add) >0):
+        myCalendar.insertListEvent(element_to_add)
+    else:
+        print("List is up-to date, no element need to be added")
+
+
+def googleOps(): #google calendar operations
+    getList()
+    updateList(listEventi)
+    #should i check if the new update has the same event list of the last one
+    #If true skip the update
+    #IF not update
+    #print("Elementi Totali : ",len(listEventi))
+    #myCalendar.clearCalendar(myCalendar.getConn())
+    #indexFrom = myCalendar.clearCalendarFromToday(myCalendar.getConn()) #clear all the events
+    #myCalendar.insertListEvent(listEventi[indexFrom:]) #add all the events
+
+
+if __name__ == '__main__':
+     #I should add a check if all events already exists
+     #IF not i should add all of them
+     #IF exists I should update only the newest
+     #while True:
+
+
+
+     print("Elis calendar Update: ",datetime.datetime.now())
+     googleOps()
+     print("Elis calendar Finish Update: ",datetime.datetime.now())
+        #time.sleep(86400) #every day repeat
+
+  
+
